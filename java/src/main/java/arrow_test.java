4c181dba18d0129f7f46a691341302b87295e510
BenWoodworth <ben@benwoodworth.net>
2018-11-06 18:06:29 -0500

    Organized Java POC code.

--- java/src/main/java/arrow_test.java
+++ java/src/main/java/arrow_test.java
@@ -1,28 +1,55 @@
-import org.apache.arrow.plasma.*;
+import org.apache.arrow.plasma.PlasmaClient;
+
+import java.nio.file.Paths;
 
 public class arrow_test {
 
-    private static void client(){
+    // Install Miniconda for Python 3
+    // To directory: ~/.local/lib/miniconda3/
+
+    // Install Arrow libraries:
+    // $ cd ~/.local/lib/miniconda3/bin/
+    // $ ./conda install arrow-cpp=0.11.* -c conda-forge
+    // $ ./conda install pyarrow=0.11.* -c conda-forge
+
+    // In Intellij, edit run configuration:
+    // VM Arguments: -Djava.library.path="~/.local/lib/miniconda3/lib/"
+
+    public static void main(String[] args) {
+        System.out.println("Java libraries path: " + System.getProperty("java.library.path"));
+
+        String[] libs = new String[]{
+                "~/.local/lib/miniconda3/pkgs/icu-58.2-hfc679d8_0/lib/libicui18n.so.58",
+                "~/.local/lib/miniconda3/pkgs/icu-58.2-hfc679d8_0/lib/libicudata.so.58",
+                "~/.local/lib/miniconda3/pkgs/boost-cpp-1.68.0-h3a22d5f_0/lib/libboost_regex.so.1.68.0",
+                "~/.local/lib/miniconda3/pkgs/boost-cpp-1.68.0-h3a22d5f_0/lib/libboost_filesystem.so.1.68.0",
+                "~/.local/lib/miniconda3/pkgs/boost-cpp-1.68.0-h3a22d5f_0/lib/libboost_system.so.1.68.0",
+                "~/.local/lib/miniconda3/pkgs/arrow-cpp-0.11.1-py36h3bd774a_0/lib/libplasma.so.11.1.0"
+        };
+
+        // Replace '~' in paths with home directory
+        String homeDir = System.getProperty("user.home");
+        for (int i = 0; i < libs.length; i++) {
+            libs[i] = libs[i].replaceFirst("^~", homeDir);
+        }
+
+        for (String lib : libs) {
+            String libAbsolutePath = Paths
+                    .get(lib)
+                    .toAbsolutePath()
+                    .toString();
+
+            System.load(libAbsolutePath);
+        }
+
+        client();
+    }
+
+    private static void client() {
         PlasmaClient plasmaClient = new PlasmaClient("/tmp/plasma", "", 0);
         byte[] byteObject = null;
 
 
 //        plasmaClient.put();
-    }
-
-    public static void main(String[] args){
-        // Edit run configuration -> VM Arguments:
-        // -Djava.library.path="~/.local/lib/miniconda3/lib/"
-        System.out.println(System.getProperty("java.library.path"));
-
-//        System.load("/home/ben/.local/lib/miniconda3/pkgs/arrow-cpp-0.11.1-py36h3bd774a_0/lib/libplasma.so.11.1.0");
-
-
-        System.loadLibrary("libplasma");
-//        System.loadLibrary("/home/ben/.local/lib/miniconda3/lib/libplasma.so.11.1.0");
-//        System.load("/home/ben/.local/lib/miniconda3/lib/libarrow.so");
-//        System.load("/home/ben/.local/lib/miniconda3/lib/libboost.so");
-
-        client();
     }
 }
