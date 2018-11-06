392c82f0c39114ddd6929febf21f8a1f1227473d
BenWoodworth <ben@benwoodworth.net>
2018-11-06 17:30:49 -0500

    Added System.load calls to java POC.

--- java/src/main/java/arrow_test.java
+++ java/src/main/java/arrow_test.java
@@ -2,15 +2,27 @@ import org.apache.arrow.plasma.*;
 
 public class arrow_test {
 
-    private void client(){
+    private static void client(){
         PlasmaClient plasmaClient = new PlasmaClient("/tmp/plasma", "", 0);
         byte[] byteObject = null;
 
 
-        plasmaClient.put();
+//        plasmaClient.put();
     }
 
     public static void main(String[] args){
+        // Edit run configuration -> VM Arguments:
+        // -Djava.library.path="~/.local/lib/miniconda3/lib/"
+        System.out.println(System.getProperty("java.library.path"));
 
+//        System.load("/home/ben/.local/lib/miniconda3/pkgs/arrow-cpp-0.11.1-py36h3bd774a_0/lib/libplasma.so.11.1.0");
+
+
+        System.loadLibrary("libplasma");
+//        System.loadLibrary("/home/ben/.local/lib/miniconda3/lib/libplasma.so.11.1.0");
+//        System.load("/home/ben/.local/lib/miniconda3/lib/libarrow.so");
+//        System.load("/home/ben/.local/lib/miniconda3/lib/libboost.so");
+
+        client();
     }
 }
