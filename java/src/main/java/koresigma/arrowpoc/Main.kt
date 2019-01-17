1c7a0e8e348982b1f9dd3f0e20e450ee97d4fd85
BenWoodworth <ben@benwoodworth.net>
2019-01-17 16:18:47 -0500

    Removed PlasmaClientTest.java and cleaned up POC code.

--- java/src/main/java/koresigma/arrowpoc/Main.kt
+++ java/src/main/java/koresigma/arrowpoc/Main.kt
@@ -1,24 +1,14 @@
 package koresigma.arrowpoc
 
-import org.apache.arrow.plasma.PlasmaClientTest
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
-    PlasmaClientTest().doTest()
-
     System.loadLibrary("plasma_java")
 
-    val pathArg: String
-
-    when (args.size) {
-        0 ->
-            pathArg = "/tmp/plasma"
-
-        1 ->
-            pathArg = args[0]
-
-        else ->
-            throw IllegalArgumentException("Takes one option argument: plasma store path")
+    val pathArg = when (args.size) {
+        0 -> "/tmp/plasma"
+        1 -> args[0]
+        else -> throw IllegalArgumentException("Takes one option argument: plasma store path")
     }
 
     val arrowPoc = ArrowPoc()
