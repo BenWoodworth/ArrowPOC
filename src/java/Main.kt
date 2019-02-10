d963dcff4557e2114963cf6b6883c8a60af46cf3
BenWoodworth <ben@benwoodworth.net>
2019-02-10 14:21:39 -0500

    Remove old code comment in Main.

--- src/java/Main.kt
+++ src/java/Main.kt
@@ -54,20 +54,3 @@ object Main {
     }
 }
 
-//fun main() {
-//
-//    val entryPoint = PythonEntryPoint { pySerializers, pyReadWriters ->
-//
-//        PlasmaStore(File("/tmp/plasma"), 1000).use { store ->
-//            val client = store.createClient()
-//
-//            val id = ByteArray(20)
-//
-//            client.put(id, "Hello, world!".toByteArray(), null)
-//            val value = client.get(id, 1000, false).toString(Charsets.UTF_8)
-//
-//            println(value)
-//        }
-//    }
-//
-//}
